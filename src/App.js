797235223f9023d5320a7bc888d393e532a92c61
Tony <nguyenphuthinh1025@gmail.com>
2024-05-30 15:37:01 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./page/Home/Home";
+import { Router, Switch, Route } from "react-router-dom";
+import {createBrowserHistory} from 'history';
+import AnNinhMang from "./page/AnNinhMang/AnNinhMang";
+import CEH from "./page/CEH/CEH";
+import AWS from "./page/AWS/AWS";
+import DangKyNgay from "./page/DangKyNgay/DangKyNgay";
+import PhatTrienPhanMem from "./page/PhatTrienPhanMem/PhatTrienPhanMem";
+import ThietKeDoHoa from "./page/ThietKeDoHoa/ThietKeDoHoa";
+import NotFound from "./page/404/NotFound";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
+;
+export const history = createBrowserHistory()
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+    <Switch>
+      <Route exact path="/an-ninh-mang" component={AnNinhMang} />
+      <Route exact path="/khoa-hoc-ceh" component={CEH} />
+      <Route exact path="/khoa-hoc-aws" component={AWS} />
+      <Route exact path="/dang-ky-ngay" component={DangKyNgay} />
+      <Route exact path="/phat-trien-phan-mem" component={PhatTrienPhanMem} />
+      <Route exact path="/thiet-ke-do-hoa" component={ThietKeDoHoa} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/" render={() => <Redirect to="/home" />} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
   );
 }
 
